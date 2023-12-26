b03bbaafaa94e766e2b012ead32af08e416e5746
Behnam Hamidi <128324799+behnamhamidi1991@users.noreply.github.com>
2023-12-26 15:53:28 +0330

    Update Featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,7 +5,7 @@ function Featured() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>What's Up?</b> Discover my stories and creative ideas.
+        <b>What is Up?</b> Discover my stories and creative ideas.
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
