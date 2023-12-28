7eb5af267d5d689c9a38fc9401d2042a6a9340b0
Behnam Hamidi <128324799+behnamhamidi1991@users.noreply.github.com>
2023-12-29 02:32:34 +0330

    Update page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -11,7 +11,7 @@ function LoginPage() {
       <div className={styles.wrapper}>
         <div className={styles.socialButton} onClick={() => signIn("google")}>
           Sign in with Google
-        </div>
+        </div> 
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
